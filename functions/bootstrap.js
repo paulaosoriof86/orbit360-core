916567cc51311775e3efbb515a00ac9069323d8b
paulaosoriof86 <paula.osorio.f86@gmail.com>
2026-08-04 11:51:21 -0600

    feat(functions): register protected tenant domain configuration

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -3,6 +3,7 @@
 module.exports = Object.assign(
   {},
   require('./user-onboarding'),
+  require('./tenant-domain-config'),
   require('./ops-leads-domain'),
   require('./cobros-reconciliation-domain'),
   require('./index'),
