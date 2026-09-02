82ca5b903bc14a14ae27478c64745e7cf517edf0
paulaosoriof86 <paula.osorio.f86@gmail.com>
2026-09-01 20:36:54 -0600

    recovery(iter2): compose product server owners

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -5,8 +5,11 @@ module.exports = Object.assign(
   require('./user-onboarding'),
   require('./tenant-domain-config'),
   require('./ops-leads-domain'),
+  require('./product-ops-leads-domain'),
   require('./ops-advisor-inbox'),
   require('./cobros-reconciliation-domain'),
+  require('./product-operational-domain'),
+  require('./product-insurer-credentials'),
   require('./recurring-insurance-import'),
   require('./index'),
   require('./bank-accounts')
